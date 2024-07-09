94a9ee62a96859a5bf472a92d76c29087e6f77b4
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-09 18:13:28 +0300

    Add project files.

---
+++ EgitimTakip.Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Models
+{
+    public class Company:BaseModel
+    {
+        public string Name { get; set; }
+
+        public int AppUserId {  get; set; }
+
+        public virtual AppUser User { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }=new List<Employee>();
+    }
+}
---
+++ EgitimTakip.Models/TrainingSubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Models
+{
+    public class TrainingSubject:BaseModel
+    {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+
+
+    }
+}
