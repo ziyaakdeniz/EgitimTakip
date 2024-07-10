3d5caf9e6e5886810d76641a4ea2975a4fadf393
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-11 01:43:40 +0300

    CompanyController Tamamlandı.

---
+++ EgitimTakip.Web/Controllers/CompanyController.cs
@@ -0,0 +1,57 @@
+﻿using EgitimTakip.Data;
+using EgitimTakip.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimTakip.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompanyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            var result=_context.Companies.Where(x=>x.IsDeleted==false).ToList();
+            return Json(new {data=result});
+        }
+        [HttpPost]
+        public IActionResult Add(Company  company)
+        {
+            _context.Companies.Add(company);
+            _context.SaveChanges();
+            return Ok(company);
+        }
+        [HttpPost]
+        public IActionResult Update(Company company)
+        {
+            _context.Companies.Update(company);
+            _context.SaveChanges();
+            return Ok(company);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Company company = _context.Companies.Find(id);
+            company.IsDeleted = true;
+            _context.Companies.Update(company);
+            _context.SaveChanges();
+            return Ok(company);
+        }
+        [HttpPost]
+        public IActionResult HardDelete(int id)
+        {
+            Company company = _context.Companies.Find(id);
+            _context.Companies.Remove(company);
+            _context.SaveChanges();
+            return Ok(company);
+        }
+    }
+}
