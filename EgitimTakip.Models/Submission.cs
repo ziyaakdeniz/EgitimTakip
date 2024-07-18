e2aaa24902f14c1f64a55ab1465c3df48f835bc3
ziya akdeniz <ziaakdeniz@gmail.com>
2024-07-18 03:43:56 +0300

    #

---
+++ EgitimTakip.Models/Submission.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Models
+{
+    public class Submission:BaseModel
+    {
+        public string? TransactionNo { get; set; }
+
+        public string? Message { get; set; }
+
+        public int EmployerId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public int TrainingId {  get; set; }
+        public virtual Training Training { get; set; }
+
+        public int TrainingSubjectId { get; set; }
+        public virtual TrainingSubject TrainingSubject { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public virtual AppUser User { get; set; }
+
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+    }
+}
